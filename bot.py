5ce8cfe87304ae445e90f853e3465ca6b481da7d
Dina <45810065+memedinner@users.noreply.github.com>
2020-12-16 05:11:00 -0500

    Done!

--- bot.py
+++ bot.py
@@ -8,14 +8,23 @@ async def on_ready():
     #confirms bot is running
     print("Bot is ready to use.")
 
-@client.command()
-async def ping(ctx):
-    #test to see if the bot works
-    await ctx.send(f"{ctx.author.mention} Hello, you pinged me.")
+# @client.command()
+# async def ping(ctx):
+#     #test to see if the bot works
+#     await ctx.send(f"{ctx.author.mention} Hello, you pinged me.")
+
 
 @client.command()
+#ALWAYS MAKE SURE TO INCLUDE THE DECORATOR ABOVE EVERY COMMAND
+#allows author to bonk the mentioned user
 async def bonk(ctx, user1: discord.Member):
     await ctx.send(f"{ctx.author.mention} has bonked {user1.mention}.")
+    
+    #attaching the image
+    image = discord.File("bonk.jpg")
+    e = discord.Embed()
+    e.set_image(url="attachment://bonk.jpg")
+    await ctx.send(file = image, embed = e )
 
 
 client.run(token)
